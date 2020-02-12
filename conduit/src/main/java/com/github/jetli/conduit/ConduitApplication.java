2048668bb53d0f0dd458131e357cceae9969c05a
Jet Li <jing.i.qin@icloud.com>
2020-02-12 12:33:28 +0800

    Add project layout

---
+++ conduit/src/main/java/com/github/jetli/conduit/ConduitApplication.java
@@ -0,0 +1,13 @@
+package com.github.jetli.conduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitApplication.class, args);
+	}
+
+}
